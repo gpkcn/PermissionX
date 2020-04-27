e16abf023f312afcba5b7545fdec1b16328e9e19
gpkcn <zfq0797@163.com>
2020-04-27 09:51:39 +0800

    fix bug commit.

--- app/src/main/java/gpk/org/permissionx/MainActivity.kt
+++ app/src/main/java/gpk/org/permissionx/MainActivity.kt
@@ -11,6 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG,"onCreate.")
+        Log.d(TAG,"onCreate. bug fix.")
     }
 }
